e0f781e19566af3a3aca276f42f5d569fe570bbd
Ellen Waddle <elwa8931@colorado.edu>
2019-10-03 19:41:29 -0600

    added tests for empty or null lists

--- test_data_viz.py
+++ test_data_viz.py
@@ -1,5 +1,6 @@
 import unittest
 import sys
+import os
 import data_viz as dv
 
 class TestDataViz(unittest.TestCase):
@@ -7,6 +8,14 @@ class TestDataViz(unittest.TestCase):
     def test_data_viz_boxplot_name_already_exists(self):
         r=dv.boxplot([1,2,3,4],'empty.txt')
         self.assertEqual(r,'this file name is taken, choose another.')
+
+    def test_data_viz_boxplot_empty_list(self):
+        with self.assertRaises(ValueError):
+            dv.boxplot([], 'test1.png', 'SMTS')
+
+    def test_data_vix_no_list(self):
+        with self.assertRaises(ValueError):
+            dv.boxplot(None, 'test1.png', 'SMTS')
 
 if __name__=='__main__':
     unittest.main()
